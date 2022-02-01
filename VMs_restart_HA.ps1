9494204a1d32be7ff082d0d270a3164905a1454e
Norman <98784491+shfy80@users.noreply.github.com>
2022-01-31 18:15:13 -0600

    Delete VMs_restart_HA.ps1

--- VMs_restart_HA.ps1
+++
@@ -1,3 +0,0 @@
-$Date = Get-Date
-$HAVMrestartold=10
-Get-VIEvent -maxsamples 6000 -Start ($Date).AddDays(-$HAVMrestartold) -type warning | Where {$_.FullFormattedMessage -match "restarted"} |select CreatedTime,FullFormattedMessage | sort CreatedTime -Descending | Export-CSV E:\norman\HA_restarted_vm.csv
